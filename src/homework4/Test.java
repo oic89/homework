f4aeb2d3e1f6fdbb78241d1a5628a8dda54f8325
czh <942940593@qq.com>
2024-03-18 20:03:11 +0800

    1.0

--- src/homework4/Test.java
+++ src/homework4/Test.java
@@ -11,11 +11,40 @@ public class Test {
     public static void main(String[] args) {
         showLastLesson();
         inputNextLesson();
+        countAll();
+    }
+
+    //计算本学期成绩
+    private static void countAll() {
+        ArrayList<String> unqualifiedLesson = new ArrayList<>();
+        int sumScore = 0;
+        double sumGPA = 0.0;
+        for (Lesson lesson : lessons) {
+            int score = lesson.getScore();
+            if (score < 60) {
+                unqualifiedLesson.add(lesson.getLessonName());
+            }
+            sumGPA += countGPA(score);
+            sumScore += score;
+        }
+        sumGPA = Math.round(sumGPA * 10.0) / 10.0;
+        double averageScore = sumScore / 10.0;
+        double averageGPA = sumGPA / 10.0;
+        System.out.println("本学年情况:");
+        System.out.println("总分:   " + sumScore + "    平均分: " + averageScore);
+        System.out.println("总绩点: " + sumGPA + "   平均绩点: " + averageGPA);
+        if (unqualifiedLesson.isEmpty()) {
+            return;
+        }
+        System.out.print("本学年不合格课程: ");
+        for (String name : unqualifiedLesson) {
+            System.out.print(name + " ");
+        }
 
     }
 
     //输入下学期目标
-    public static void inputNextLesson(){
+    private static void inputNextLesson() {
         System.out.println("请输入下学期目标");
         System.out.print("高等数学Ⅱ:");
         lessons.add(new Lesson("高等数学Ⅱ", sc.nextInt(), 1));
@@ -32,34 +61,35 @@ public class Test {
     }
 
     //初始课程
-    public static void showLastLesson() {
-        lessons.add(new Lesson("高等数学Ⅰ",87,0));
-        lessons.add(new Lesson("大学英语Ⅰ",56,0));
-        lessons.add(new Lesson("管理学原理",78,0));
-        lessons.add(new Lesson("程序设计",97,0));
-        lessons.add(new Lesson("体育",93,0));
+    private static void showLastLesson() {
+        lessons.add(new Lesson("高等数学Ⅰ", 87, 0));
+        lessons.add(new Lesson("大学英语Ⅰ", 56, 0));
+        lessons.add(new Lesson("管理学原理", 78, 0));
+        lessons.add(new Lesson("程序设计", 97, 0));
+        lessons.add(new Lesson("体育", 93, 0));
         showLesson(0);
     }
 
-    public static void showLesson(int state){
-        if (state==0){
+    private static void showLesson(int state) {
+        if (state == 0) {
             System.out.println("上学期课程:");
-        }else {
+        } else {
             System.out.println("下学期课程:");
         }
-        int sumScore=0;
-        double sumGPA=0.0;
+        int sumScore = 0;
+        double sumGPA = 0.0;
         for (Lesson lesson : lessons) {
-            if (lesson.getState()!=state){
+            if (lesson.getState() != state) {
                 continue;
             }
-            double GPA=countGPA(lesson.getScore());
-            sumScore+= lesson.getScore();
-            sumGPA+=GPA;
-            System.out.println(lesson.getLessonName() + "  得分: " + lesson.getScore() +"  绩点: "+GPA);
+            double GPA = countGPA(lesson.getScore());
+            sumScore += lesson.getScore();
+            sumGPA += GPA;
+            System.out.println(lesson.getLessonName() + "  得分: " + lesson.getScore() + "  绩点: " + GPA);
         }
-        double averageScore=sumScore/5.0;
-        double averageGPA=sumGPA/5.0;
+        sumGPA = Math.round(sumGPA * 10.0) / 10.0;
+        double averageScore = sumScore / 5.0;
+        double averageGPA = sumGPA / 5.0;
         System.out.println("------------------------------");
         System.out.println("总分:   " + sumScore + "    平均分: " + averageScore);
         System.out.println("总绩点: " + sumGPA + "   平均绩点: " + averageGPA);
@@ -67,10 +97,10 @@ public class Test {
     }
 
     //计算绩点
-    private static double countGPA(int score){
-        double GPA=0;
-        if (score>=60){
-            GPA=1.0+(score-60)/10.0;
+    private static double countGPA(int score) {
+        double GPA = 0;
+        if (score >= 60) {
+            GPA = 1.0 + (score - 60) / 10.0;
         }
         return GPA;
     }
