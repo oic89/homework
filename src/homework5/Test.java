05b58f4020dd349d11622e6eac39be23f2e11ced
czh <942940593@qq.com>
2024-04-15 13:05:17 +0800

    problem5

--- src/homework5/Test.java
+++ src/homework5/Test.java
@@ -11,21 +11,24 @@ public class Test {
         System.out.println("面积: " + triangle.S());
         System.out.println("内切圆半径: " + triangle.inCircleRadius());
         System.out.println("重心坐标: " + triangle.centroid());
-        System.out.println();
         // 创建矩形
-        System.out.println("矩形:");
-        Rectangle rectangle = new Rectangle(new Point(0, 0), 2, 2);
-        System.out.println("两边长为: "+rectangle.width+" , "+ rectangle.height);
-        System.out.println("周长: " + rectangle.C());
-        System.out.println("面积: " + rectangle.S());
-        if (rectangle.isSquare()) {
-            Circle incircle = rectangle.incircle();
-            Circle circumcircle = rectangle.circumcircle();
-            System.out.println("该矩形为正方形");
-            System.out.println("内切圆: " + "圆心"+incircle.center+ " 半径 "+incircle.getRadius()+ "  周长 "+incircle.C()+ " 面积 "+incircle.S());
-            System.out.println("外接圆: " + "圆心"+circumcircle.center+ " 半径 "+circumcircle.getRadius()+ " 周长 "+circumcircle.C()+ " 面积 "+circumcircle.S());
-        } else {
-            System.out.println("这不是正方形");
+        System.out.println("\n矩形:");
+        Rectangle rectangle = new Rectangle(new Point(0, 0), 2, 3);
+        while (true) {
+            System.out.println("两边长为: "+rectangle.width+" , "+ rectangle.height);
+            System.out.println("周长: " + rectangle.C());
+            System.out.println("面积: " + rectangle.S());
+            if (rectangle.isSquare()) {
+                Circle incircle = rectangle.incircle();
+                Circle circumcircle = rectangle.circumcircle();
+                System.out.println("该矩形为正方形");
+                System.out.println("内切圆: " + "圆心"+incircle.center+ " 半径 "+incircle.getRadius()+ "  周长 "+incircle.C()+ " 面积 "+incircle.S());
+                System.out.println("外接圆: " + "圆心"+circumcircle.center+ " 半径 "+circumcircle.getRadius()+ " 周长 "+circumcircle.C()+ " 面积 "+circumcircle.S());
+                return;
+            } else {
+                System.out.println("这不是正方形\n");
+                rectangle = new Rectangle(new Point(0, 0), 2, 2);
+            }
         }
     }
 }
